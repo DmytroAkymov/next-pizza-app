d2214df4d64d11e0545a9ba9c3f4cd6d0fd9a8dd
DmytroAkymov <2dmitri.akimov@gmail.com>
2025-01-12 14:48:58 +0100

    search-input-products

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -21,28 +21,24 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
     setFocused(false);
   });
 
-  //   useDebounce(
-  //     async () => {
-  //       try {
-  //         const response = await Api.products.search(searchQuery);
-  //         setProducts(response);
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     },
-  //     250,
-  //     [searchQuery]
-  //   );
+  useDebounce(
+    async () => {
+      try {
+        const response = await Api.products.search(searchQuery);
+        setProducts(response);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    250,
+    [searchQuery]
+  );
 
   const onClickItem = () => {
     setFocused(false);
     setSearchQuery("");
     setProducts([]);
   };
-
-  React.useEffect(() => {
-    Api.products.search(searchQuery);
-  }, [searchQuery]);
 
   return (
     <>
@@ -65,28 +61,30 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <div
-          className={cn(
-            "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
-            focused && "visible opacity-100 top-12"
-          )}
-        >
-          {products.map((product) => (
-            <Link
-              onClick={onClickItem}
-              key={product.id}
-              className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
-              href={`/product/${product.id}`}
-            >
-              <img
-                className="rounded-sm h-8 w-8"
-                src={product.imageUrl}
-                alt={product.name}
-              />
-              <span>{product.name}</span>
-            </Link>
-          ))}
-        </div>
+        {products.length > 0 && (
+          <div
+            className={cn(
+              "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
+              focused && "visible opacity-100 top-12"
+            )}
+          >
+            {products.map((product) => (
+              <Link
+                onClick={onClickItem}
+                key={product.id}
+                className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+                href={`/product/${product.id}`}
+              >
+                <img
+                  className="rounded-sm h-8 w-8"
+                  src={product.imageUrl}
+                  alt={product.name}
+                />
+                <span>{product.name}</span>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
--- services/products.ts
+++ services/products.ts
@@ -1,10 +1,10 @@
 import { Product } from "@prisma/client";
 import { axiosInstance } from "./instance";
+import { ApiRoutes } from "./constants";
 
-export const search = async (query: string): Promise<Product[]> => {
-  return (
-    await axiosInstance.get<Product[]>("/products/search", {
-      params: { query },
-    })
-  ).data;
+export const search = async (searchQuery: string): Promise<Product[]> => {
+  const response = await axiosInstance.get(ApiRoutes.SEARCH_PRODUCTS, {
+    params: { query: searchQuery },
+  });
+  return response.data;
 };
