bacdf5ff2ed81aae376945c53d6cb4ebf3bce4a8
DmytroAkymov <2dmitri.akimov@gmail.com>
2025-01-30 16:19:03 +0100

    product-form

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,8 +1,7 @@
-import { Container, Title } from "@/shared/components/shared";
-import { GroupVariants } from "@/shared/components/shared/group-variants";
-import { PizzaImage } from "@/shared/components/shared/pizza-image";
+import { Container } from "@/shared/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
+import { ProductForm } from "@/shared/components/shared/product-form";
 
 export default async function ProductPage({
   params: { id },
@@ -31,40 +30,7 @@ export default async function ProductPage({
   }
   return (
     <Container className="flex flex-col my-10">
-      <div className="flex flex-1">
-        <PizzaImage imageUrl={product.imageUrl} size={40} />
-        <div className="w-[490px] bg-[#fcfcfc] p-7">
-          <Title
-            text={product.name}
-            size="md"
-            className="font-extrabold mb-1"
-          />
-          <p className="text-gray-400">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Exercitationem reiciendis consectetur corporis voluptas aliquam
-            nihil? Aliquam sequi amet possimus velit, quas ut, similique error
-            cupiditate magnam suscipit laudantium officiis perferendis?
-          </p>
-          <GroupVariants
-            value="2"
-            items={[
-              {
-                name: "Small",
-                value: "1",
-              },
-              {
-                name: "Average",
-                value: "2",
-              },
-              {
-                name: "Big",
-                value: "3",
-                disabled: true,
-              },
-            ]}
-          />
-        </div>
-      </div>
+      <ProductForm product={product} />
     </Container>
   );
 }
